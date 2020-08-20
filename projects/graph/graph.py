46c95e4755ac96b62442e50834f17cb10868bee3
elijahmandeville <elijahmandeville@gmail.com>
2020-08-20 17:16:22 -0600

    finished everything for part 1

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,9 +3,10 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
-class Graph:
 
+class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
@@ -13,42 +14,94 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        # Create the new key with the vertex ID, and set the value to an empty set (meaning no edges yet)
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        # Find vertex V1 in our vertices, add V2 to the set of edges
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # Create an empty queue and enqueue the starting_vertex
+        queue = Queue()
+        queue.enqueue(starting_vertex)
+        # Create an empty set to track visited verticies
+        visited = set()
+        # while the queue is not empty:
+        while queue.size() > 0:
+            # get current vertex (dequeue from queue)
+            current = queue.dequeue()
+            # Check if the current vertex has not been visited:
+            if current not in visited:
+                # print the current vertex
+                print(current)
+                # Mark the current vertex as visited
+                visited.add(current)
+                # queue up all the current vertex's neighbors (so we can visit them next)
+                for el in self.get_neighbors(current):
+                    queue.enqueue(el)
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # Create an empty stack and add the starting_vertex
+        s = Stack()
+        s.push(starting_vertex)
+        # Create an empty set to track visited verticies
+        visited = set()
+        # while the stack is not empty:
+        while s.size() > 0:
+            # get current vertex (pop from stack)
+            current = s.pop()
+            # Check if the current vertex has not been visited:
+            if current not in visited:
+                # print the current vertex
+                print(current)
+                # Mark the current vertex as visited
+                # Add the current vertex to a visited_set
+                visited.add(current)
+                # push up all the current vertex's neighbors (so we can visit them next
+                for el in self.get_neighbors(current):
+                    s.push(el)
 
-    def dft_recursive(self, starting_vertex):
+    def dft_recursive(self, starting_vertex, visited=None):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
-
         This should be done using recursion.
         """
-        pass  # TODO
+        if visited is None:
+            visited = set()
+
+        visited.add(starting_vertex)
+
+        print(starting_vertex)
+
+        edges = self.get_neighbors(starting_vertex)
+
+        if len(edges) <= 0:
+            return
+
+        for edge in edges:
+            if edge not in visited:
+                self.dft_recursive(edge, visited)
 
     def bfs(self, starting_vertex, destination_vertex):
         """
@@ -56,7 +109,32 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        # Create an empty queue and enqueue the PATH TO starting_vertex
+        q = Queue()
+        q.enqueue([starting_vertex])
+
+        # Create an empty set to track visited verticies
+        visited = set()
+        # while the queue is not empty:
+        while q.size() > 0:
+            # get current vertex PATH (dequeue from queue)
+            current_path = q.dequeue()
+            # set the current vertex to the LAST element of the PATH
+            current_vertex = current_path[-1]
+            # CHECK IF THE CURRENT VERTEX IS DESTINATION
+            if current_vertex is destination_vertex:
+                # IF IT IS, STOP AND RETURN
+                return current_path
+            # Check if the current vertex has not been visited:
+            if current_vertex not in visited:
+                visited.add(current_vertex)
+
+                edges = self.get_neighbors(current_vertex)
+
+                for edge in edges:
+                    clone = list(current_path)
+                    clone.append(edge)
+                    q.enqueue(clone)
 
     def dfs(self, starting_vertex, destination_vertex):
         """
@@ -64,17 +142,57 @@ class Graph:
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        pass  # TODO
+        s = Stack()
+        s.push([starting_vertex])
 
-    def dfs_recursive(self, starting_vertex, destination_vertex):
+        visited = set()
+
+        while s.size() > 0:
+            current_path = s.pop()
+
+            current_vertex = current_path[-1]
+
+            if current_vertex is destination_vertex:
+                return current_path
+
+            if current_vertex not in visited:
+                visited.add(current_vertex)
+
+                edges = self.get_neighbors(current_vertex)
+
+                for edge in edges:
+                    clone = list(current_path)
+                    clone.append(edge)
+                    s.push(clone)
+
+    def dfs_recursive(self, starting_vertex, destination_vertex, path=None, visited=None):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
         depth-first order.
-
         This should be done using recursion.
         """
-        pass  # TODO
+        if path is None:
+            path = []
+
+        if visited is None:
+            visited = set()
+
+        visited.add(starting_vertex)
+        path = path + [starting_vertex]
+
+        if starting_vertex == destination_vertex:
+            return path
+
+        for edge in self.get_neighbors(starting_vertex):
+            if edge not in visited:
+                new_path = self.dfs_recursive(
+                    edge, destination_vertex, path, visited)
+                if new_path:
+                    return new_path
+            else:
+                return None
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
@@ -96,13 +214,11 @@ if __name__ == '__main__':
     graph.add_edge(3, 5)
     graph.add_edge(2, 3)
     graph.add_edge(4, 6)
-
     '''
     Should print:
         {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
     '''
     print(graph.vertices)
-
     '''
     Valid BFT paths:
         1, 2, 3, 4, 5, 6, 7
@@ -119,7 +235,6 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 5, 6
     '''
     graph.bft(1)
-
     '''
     Valid DFT paths:
         1, 2, 3, 5, 4, 6, 7
@@ -127,19 +242,17 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
-    graph.dft(1)
-    graph.dft_recursive(1)
-
+    # graph.dft(1)
+    # graph.dft_recursive(1)
     '''
     Valid BFS path:
         [1, 2, 4, 6]
     '''
-    print(graph.bfs(1, 6))
-
+    # print(graph.bfs(1, 6))
     '''
     Valid DFS paths:
         [1, 2, 4, 6]
         [1, 2, 4, 7, 6]
     '''
-    print(graph.dfs(1, 6))
-    print(graph.dfs_recursive(1, 6))
+    # print(graph.dfs(1, 6))
+    # print(graph.dfs_recursive(1, 6))
